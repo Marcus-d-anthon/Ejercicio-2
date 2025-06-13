36c70ae86bb76493d06f6b48b4eb6ca2657a7af1
Elías Daniel Bone <08elidan2005@gmail.com>
2025-06-12 22:48:21 -0500

    Add files via upload

---
+++ Perro.js
@@ -0,0 +1,12 @@
+/*2. Crear un objeto con propiedades y un método
+Ejercicio: Crea un objeto llamado mascota que tenga las propiedades nombre, tipo y un método hablar() que imprima un mensaje como "Guau! Soy Firulais".*/
+
+const mascota = {
+  nombre: "Armando",
+  tipo: "Rottweiler",
+  hablar: () => {
+    console.log("¡Guau! Soy Firulais");
+  }
+};
+
+mascota.hablar();
